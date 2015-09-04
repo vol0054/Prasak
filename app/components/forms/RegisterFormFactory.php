e0e4da614e2ede800ba4b1e6521a0e1ca621a82f
KytaVeprova <KytaVeprova@localhost.localdomain>
2015-09-04 20:23:15 +0200

    modified register form - added numebr of runners in one team

--- app/components/forms/RegisterFormFactory.php
+++ app/components/forms/RegisterFormFactory.php
@@ -12,12 +12,15 @@ class RegisterFormFactory {
     public function create(){
         
         $form = new Form;
-        $form->addText('nazev','nazev tymu: ')
-                ->setRequired('vyplnte prosim jmeno vaseho tymu');
+        $form->addText('nazev','název tymu: ')
+                ->setRequired('vyplňte prosím jméno vašeho týmu');
+		$form->addText('pocetUcastniku', 'počet závodníků ve Vašem týmu:')
+				->setRequired('Zadejte prosím počet členů Vašeho tým');
+				/** @TODO add validator - number of runners must be lower than <5 */
         $form->addText('email','kontaktni email: ')
                 ->setRequired('neplatny email');
         $form->addCheckbox('terms')
-                ->setRequired();
+                ->setRequired()->setOmitted(TRUE);
         
         $form->addSubmit('submit','Registrovat');
 	
